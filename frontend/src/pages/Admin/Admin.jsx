36d030a103acf4d48e8ef1898c6c4c34a97483f9
Copeta1 <pecolajrobert1@gmail.com>
2025-11-23 12:05:00 +0100

    fix

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import ManageDishes from "./ManageDishes";
 import Orders from "./Orders";
 import Permissions from "./Permissions";
-import AddDishes from "./addDishes";
+import AddDishes from "./AddDishes";
 
 const Admin = () => {
   const [selectedMenu, setSelectedMenu] = useState("manageDishes");
